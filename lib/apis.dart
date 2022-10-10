6c14c78c916fe9680f5cbf591e4c185b0aaf370f
m-vishnuraj <mvishnurajm@gmail.com>
2022-10-10 08:19:35 +0530

    http testing completed

--- lib/apis.dart
+++ lib/apis.dart
@@ -1,7 +1,6 @@
 import 'package:http/http.dart' as http;
-import 'dart:developer';
 
 Future<void> getNumberFact({required int number}) async {
   final response = await http.get(Uri.parse('http://numbersapi.com/19?json'));
-  log(response.body);
+  print(response.body);
 }
